7e059462ec094d1eff2143c24c13cdbaba250860
JeWan Jeong <jewan8821@naver.com>
2022-03-20 21:06:26 +0900

    django course 32 commit

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateViews
+from profileapp.views import ProfileCreateViews, ProfileUpdateView
 
 app_name = "profileapp"
 
 urlpatterns = [
     path('create/', ProfileCreateViews.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
